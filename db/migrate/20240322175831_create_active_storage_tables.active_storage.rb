e7151e94d6bc434583b58f75a9679fcb4608cacc
AiHughesToo <alex@aihughes.dev>
2024-03-22 15:20:44 -0400

    active storage

---
+++ db/migrate/20240322175831_create_active_storage_tables.active_storage.rb
@@ -0,0 +1,6 @@
+# This migration comes from active_storage (originally 20170806125915)
+class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
+  def change
+    # Use Active Record's configured type for primary and foreign keys
+  end
+end
